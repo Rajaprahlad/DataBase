62542c60e079f7b4fb3f5e17d3bab8a1c7bf048b
RajaPrahlad <npreddy3339@gmail.com>
2017-01-22 00:33:15 -0500

    Initial Commit

---
+++ DataBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataBase
+//
+//  Created by Prahlad on 1/22/17.
+//  Copyright © 2017 Prahlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
